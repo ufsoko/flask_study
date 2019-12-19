302d69510e04bed1ffe9932c4a44bfdca8a4e9ba
gototrying <gototrying@163.com>
2019-12-19 21:31:40 +0800

    test add kogin

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -61,3 +61,12 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return render_template(url_for("auth.login"))
+
+        return view(**kwargs)
+    return wrapped_view
