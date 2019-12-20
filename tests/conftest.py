0f1c1106366f4a4be84cb9646d5c9592bf24935f
ufsoko <1085699796@qq.com>
2019-12-20 16:03:09 +0800

    add steup

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import os 
+import tempfile
+
+import pytest
+from flaskr import create_app
+from flaskr.db import get_db,init_db
+
+with open(os.path.join(os.path.dirname(__file__),''))
+
+@pytest.fixture
+def app():
+    pass
